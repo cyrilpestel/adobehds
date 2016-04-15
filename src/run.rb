e4c44f6764ef60a039f73042f93408ea13f447e1
Cyril Pestel <cpestel@latinus.local>
2016-04-15 15:49:30 +0200

    Adds ffmpeg video converter and dwl from url

--- src/run.rb
+++ src/run.rb
@@ -1,15 +1,21 @@
 #!/usr/bin/ruby
 
 require 'optparse'
+require 'mkmf'
 
 options = {}
 season = nil
+cmd = nil
 
 OptionParser.new do |opts|
   opts.banner = "Usage: run.rb [options]"
 
   opts.on("-c", "--cmd CMD", "Command line with AdobeHDS") do |cmd|
     options[:cmd] = cmd
+  end
+
+  opts.on("-u", "--url URL", "URL to the manifest") do |url|
+    options[:url] = url
   end
 
   opts.on("-t", "--title TITLE", "The title of the Episode") do |title|
@@ -44,7 +50,11 @@ end.parse!
 # p options
 # p ARGV
 
-if options[:cmd] == nil
+if options[:url] != nil
+  cmd = "php AdobeHDS.php --delete --manifest \"#{options[:url]}\" --auth \"#{options[:url]}&als=0,2,0,1,0,NaN,0,0,0,36,f,0,660,f,s,UDIIHDKRBYRM,2.11.3,36\" --useragent \"Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/49.0.2623.110 Safari/537.36\""
+end
+ 
+if cmd == nil && options[:cmd] == nil
 	p "Command line with AdobeHDS missing"
 	exit 1
 end
@@ -65,15 +75,26 @@ if options[:season] == nil
 	options[:season] = "01"
 end
 
-system "#{options[:cmd]}"
+system "#{cmd}"
 
 # # Looking for the filename
 files_sorted_by_time = Dir['*'].sort_by{ |f| File.mtime(f) }
 temp_filename = files_sorted_by_time.last
 puts "#{temp_filename}"
 
-puts "exec : 'avconv -i #{temp_filename} -codec copy \"#{options[:title]} - S#{options[:season]}E#{options[:episode]}.mp4\"'"
-system "avconv -i #{temp_filename} -codec copy \"#{options[:title]} - S#{options[:season]}E#{options[:episode]}.mp4\""
+avconv_path = find_executable 'avconv'
+ffmpeg_path = find_executable 'ffmpeg'
+if avconv_path != nil
+  puts "exec : 'avconv -i #{temp_filename} -codec copy \"#{options[:title]} - S#{options[:season]}E#{options[:episode]}.mp4\"'"
+  system "avconv -i #{temp_filename} -codec copy \"#{options[:title]} - S#{options[:season]}E#{options[:episode]}.mp4\""
+elsif ffmpeg_path != nil
+  puts "exec : 'ffmpeg -i #{temp_filename} -c copy -copyts \"#{options[:title]} - S#{options[:season]}E#{options[:episode]}.mp4\"'"
+  system "ffmpeg -i #{temp_filename} -c copy -copyts \"#{options[:title]} - S#{options[:season]}E#{options[:episode]}.mp4\""
+else
+    puts "Converter (avconv or ffmpeg) not found"
+    exit 1
+end
+
 
 # Print file created
 files_sorted_by_time = Dir['*'].sort_by{ |f| File.mtime(f) }
@@ -83,7 +104,7 @@ puts "#{output_filename} created"
 system "rm -f #{temp_filename}"
 
 if options[:tvshow] != nil && options[:tvshowid] != nil
-	File.open("../SearchTF1/#{options[:tvshowid]}.txt", 'a') { |file| 
+	File.open("#{options[:tvshowid]}.txt", 'a') { |file| 
 		position = "before"
 		if options[:after] != nil
 			position = "after"
